20e9667799383b0ca782d63fd93b27b2efd4ce8d
fado <pdonnelly19@qub.ac.uk>
2014-06-24 14:13:35 +0100

    Changed method signatures to reflect changes to Tool interface.

--- src/gui/tools/DefaultPointer.java
+++ src/gui/tools/DefaultPointer.java
@@ -1,21 +1,22 @@
 package gui.tools;
 
 import gui.MapSquare;
+import java.awt.event.MouseEvent;
 
 public class DefaultPointer implements Tool {
 
     @Override
-    public void mouseEntered(MapSquare cell) {
+    public void mouseEntered(MapSquare cell, MouseEvent event) {
         
     }
 
     @Override
-    public void mouseExited(MapSquare cell) {
+    public void mouseExited(MapSquare cell, MouseEvent event) {
         
     }
 
     @Override
-    public void mouseClicked(MapSquare cell) {
+    public void mouseClicked(MapSquare cell, MouseEvent event) {
         
     }
     
