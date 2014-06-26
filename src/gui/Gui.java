df172edca68a470c31d0371682cf80a42733dc0c
fado <pdonnelly19@qub.ac.uk>
2014-06-26 09:19:26 +0100

    Removed some comments for clarity.

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -12,11 +12,12 @@ import javax.swing.UnsupportedLookAndFeelException;
  * The Gui class specifies the user interface for JDig.
  */
 public class Gui implements Runnable {
-    
+
     private final int GRID_SIZE = 40;
-    
+
     /**
      * Adds components to the passed-in Container.
+     *
      * @param pane - The Container to which the components are to be added.
      */
     private void addComponentsToPane(Container pane) throws IOException {
@@ -25,31 +26,29 @@ public class Gui implements Runnable {
         constraints.weightx = 0.1;
         constraints.weighty = 0.1;
         constraints.fill = GridBagConstraints.BOTH;
-        
-        // Add MapGrid.
+
         MapGrid grid = new MapGrid(GRID_SIZE, GRID_SIZE);
         constraints.gridx = 0;
         constraints.gridy = 1;
         pane.add(grid, constraints);
-        
-        // Add MapEditorToolbar.
+
         MapEditorToolbar toolbar = new MapEditorToolbar(grid);
         constraints.gridx = 0;
         constraints.gridy = 0;
         pane.add(toolbar, constraints);
     }
-    
+
     /**
      * Creates a Gui object.
      */
-    private void createAndShowGui() throws IOException {   
+    private void createAndShowGui() throws IOException {
         JFrame frame = new JFrame("JDig");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         addComponentsToPane(frame.getContentPane());
         frame.setVisible(true);
         frame.pack();
     }
-    
+
     /**
      * Sets the look and feel to the system default, then creates a Gui object.
      */
@@ -58,7 +57,7 @@ public class Gui implements Runnable {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
             createAndShowGui();
-        } catch (ClassNotFoundException | InstantiationException | 
+        } catch (ClassNotFoundException | InstantiationException |
                 IllegalAccessException | UnsupportedLookAndFeelException |
                 IOException ex) {
             // TO-DO: Stuff.
