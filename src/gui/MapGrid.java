7c599234b166993f81957b8520588ba81551fb77
fado <pdonnelly19@qub.ac.uk>
2014-06-26 09:18:40 +0100

    Changed comments and Javadoc for clarity.

--- src/gui/MapGrid.java
+++ src/gui/MapGrid.java
@@ -10,54 +10,57 @@ import java.util.List;
 import javax.swing.JPanel;
 
 /**
- * The MapGrid class specifies a collection of MapSquare that comprise the user 
+ * The MapGrid class specifies a collection of MapSquare that comprise the user
  * interface for drawing maps within JDig.
  */
 public class MapGrid extends JPanel {
-    
+
     private Tool selectedTool;
-    private final List<MapSquare> squares;
-    private MapSquare defaultMapSquare;
-    
+    private List<MapSquare> squares;
+    private final MapSquare defaultMapSquare;
+
     /**
-     * Creates a Grid object with its fields set to the passed-in arguments.
+     * Creates a Grid object with its fields set to the passed-in arguments.  Creates
+     * a GridBagLayout maxRows by maxColumns in size and adds a new MapSquare to each
+     * cell.  A List of MapSquares is also maintained so they can be more easily
+     * accessed by other methods.
+     *
      * @param maxRows - Size of the grid on the y axis.
      * @param maxColumns - Size of the grid on the x axis.
      */
     public MapGrid(int maxRows, int maxColumns) {
         // Create a default MapSquare at position -1x, -1y.
         defaultMapSquare = new MapSquare(this, -1, -1);
-        
-        // Set default tool selection.
         selectedTool = new DefaultPointer();
-        
-        // Setup grid.
         setLayout(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
-        
-        squares = new ArrayList<>();
-        
-        for(int row = 0; row < maxRows; row++) {
-            for(int column = 0; column < maxColumns; column++) {
+
+        if (squares == null) {
+            squares = new ArrayList<>();
+        }
+
+        for (int row = 0; row < maxRows; row++) {
+            for (int column = 0; column < maxColumns; column++) {
                 constraints.gridx = column;
                 constraints.gridy = row;
-                
+
                 MapSquare square = new MapSquare(this, row, column);
-                squares.add(square);  // Gives us an easy way to access MapSquares.
+                squares.add(square);
                 add(square, constraints);
             }
         }
     }
-    
+
     /**
-     * Searches through all MapSquares in the MapGrid and returns whatever square
-     * matches the passed in Point.
+     * Searches through all MapSquares in the MapGrid and returns whatever
+     * square matches the passed in Point.
+     *
      * @param point - The location of the MapSquare being searched for.
      * @return - Returns the MapSquare if found, otherwise null.
      */
     public MapSquare getCellAt(Point point) {
-        for(MapSquare cell : squares) {
-            if(cell.getPosition() == point) {
+        for (MapSquare cell : squares) {
+            if (cell.getPosition() == point) {
                 return cell;
             }
         }
@@ -66,129 +69,148 @@ public class MapGrid extends JPanel {
 
     /**
      * Find and return the MapSquare to the west (x-1) of the specified Point.
+     *
      * @param point - The reference Point.
      * @return - The MapSquare to the west (x-1) of the specified Point.
      */
     public MapSquare getSquareWestOf(Point point) {
-        for(MapSquare square : squares) {
-           if(square.getPosition().x == point.x - 1 && square.getPosition().y == point.y) {
-               return square;
-           }
+        for (MapSquare square : squares) {
+            if (square.getPosition().x == point.x - 1 && square.getPosition().y == point.y) {
+                return square;
+            }
         }
         return defaultMapSquare;
     }
-    
+
     /**
      * Find and return the MapSquare to the east (x+1) of the specified Point.
+     *
      * @param point - The reference Point.
-     * @return  - The MapSquare to the east (x+1) of the specified Point.
+     * @return - The MapSquare to the east (x+1) of the specified Point.
      */
     public MapSquare getSquareEastOf(Point point) {
-        for(MapSquare square : squares) {
-           if(square.getPosition().x == point.x + 1 && square.getPosition().y == point.y) {
-               return square;
-           }
+        for (MapSquare square : squares) {
+            if (square.getPosition().x == point.x + 1 && square.getPosition().y == point.y) {
+                return square;
+            }
         }
         return defaultMapSquare;
     }
-    
+
     /**
      * Find and return the MapSquare to the north (y-1) of the specified Point.
+     *
      * @param point - The reference Point.
      * @return - The MapSquare to the north (y-1) of the specified Point.
      */
     public MapSquare getSquareNorthOf(Point point) {
-        for(MapSquare square : squares) {
-           if(square.getPosition().x == point.x && square.getPosition().y == point.y - 1) {
-               return square;
-           }
+        for (MapSquare square : squares) {
+            if (square.getPosition().x == point.x && square.getPosition().y == point.y - 1) {
+                return square;
+            }
         }
         return defaultMapSquare;
     }
-    
+
     /**
      * Find and return the MapSquare to the south (y+1) of the specified Point.
+     *
      * @param point - The reference Point.
      * @return - The MapSquare to the south (y+1) of the specified Point.
      */
     public MapSquare getSquareSouthOf(Point point) {
-        for(MapSquare square : squares) {
-           if(square.getPosition().x == point.x && square.getPosition().y == point.y + 1) {
-               return square;
-           }
+        for (MapSquare square : squares) {
+            if (square.getPosition().x == point.x && square.getPosition().y == point.y + 1) {
+                return square;
+            }
         }
         return defaultMapSquare;
     }
-    
+
     /**
-     * Find and return the MapSquare to the northwest (x-1, y-1) of the specified Point.
+     * Find and return the MapSquare to the northwest (x-1, y-1) of the
+     * specified Point.
+     *
      * @param point - The reference Point.
-     * @return - The MapSquare to the northwest (x-1, y-1) of the specified Point.
+     * @return - The MapSquare to the northwest (x-1, y-1) of the specified
+     * Point.
      */
     public MapSquare getSquareNorthwestOf(Point point) {
-        for(MapSquare square : squares) {
-           if(square.getPosition().x == point.x - 1 && square.getPosition().y == point.y - 1) {
-               return square;
-           }
+        for (MapSquare square : squares) {
+            if (square.getPosition().x == point.x - 1 && square.getPosition().y == point.y - 1) {
+                return square;
+            }
         }
         return defaultMapSquare;
     }
-    
+
     /**
-     * Find and return the MapSquare to the northeast (x+1, y-1) of the specified Point.
+     * Find and return the MapSquare to the northeast (x+1, y-1) of the
+     * specified Point.
+     *
      * @param point - The reference Point.
-     * @return - The MapSquare to the northeast (x+1, y-1) of the specified Point.
+     * @return - The MapSquare to the northeast (x+1, y-1) of the specified
+     * Point.
      */
     public MapSquare getSquareNortheastOf(Point point) {
-        for(MapSquare square : squares) {
-           if(square.getPosition().x == point.x + 1 && square.getPosition().y == point.y - 1) {
-               return square;
-           }
+        for (MapSquare square : squares) {
+            if (square.getPosition().x == point.x + 1 && square.getPosition().y == point.y - 1) {
+                return square;
+            }
         }
         return defaultMapSquare;
     }
+
     /**
-     * Find and return the MapSquare to the southwest (x-1, y+1) of the specified Point.
+     * Find and return the MapSquare to the southwest (x-1, y+1) of the
+     * specified Point.
+     *
      * @param point - The reference Point.
-     * @return - The MapSquare to the southwest (x-1, y+1) of the specified Point.
+     * @return - The MapSquare to the southwest (x-1, y+1) of the specified
+     * Point.
      */
     public MapSquare getSquareSouthwestOf(Point point) {
-        for(MapSquare square : squares) {
-           if(square.getPosition().x == point.x - 1 && square.getPosition().y == point.y + 1) {
-               return square;
-           }
+        for (MapSquare square : squares) {
+            if (square.getPosition().x == point.x - 1 && square.getPosition().y == point.y + 1) {
+                return square;
+            }
         }
         return defaultMapSquare;
     }
-    
+
     /**
-     * Find and return the MapSquare to the southeast (x+1, y+1) of the specified Point.
+     * Find and return the MapSquare to the southeast (x+1, y+1) of the
+     * specified Point.
+     *
      * @param point - The reference Point.
-     * @return - The MapSquare to the southeast (x+1, y+1) of the specified Point.
+     * @return - The MapSquare to the southeast (x+1, y+1) of the specified
+     * Point.
      */
     public MapSquare getSquareSoutheastOf(Point point) {
-        for(MapSquare square : squares) {
-           if(square.getPosition().x == point.x + 1 && square.getPosition().y == point.y + 1) {
-               return square;
-           }
+        for (MapSquare square : squares) {
+            if (square.getPosition().x == point.x + 1 && square.getPosition().y == point.y + 1) {
+                return square;
+            }
         }
         return defaultMapSquare;
     }
-    
+
     /**
      * Returns the currently selected tool.
+     *
      * @return - The currently selected tool.
      */
     public Tool getSelectedTool() {
         return this.selectedTool;
     }
-    
+
     /**
      * Sets the currently selected tool.
+     *
      * @param tool - The currently selected tool.
      */
     public void setSelectedTool(Tool tool) {
         this.selectedTool = tool;
     }
-   
+
 }
