cd5b024ed3e874d6749584c463b0ab1f5b4bb640
fado <pdonnelly19@qub.ac.uk>
2014-06-29 21:24:33 +0100

    Added Javadocs that may or may not be helpful.

--- src/main/Map.java
+++ src/main/Map.java
@@ -20,7 +20,19 @@ public class Map {
         }
     }
     
-    public Cell getCellRelativeTo(Cell cell, Direction direction) {
+    /**
+     * Gets the Cell adjacent to the passed-in Cell, in the passed-in Direction.
+     * Each member of the Direction enum knows the offsets that should be applied
+     * to the X and Y coordinates of the passed-in Cell to get the coordinates of the 
+     * Cell that lies adjacent in that Direction.  This method returns that Cell
+     * if it exists, otherwise it returns a default cell with the coordinates (-1, -1).
+     * 
+     * @param cell - The Cell that is the point of reference.
+     * @param direction - Specifies in which direction relative to the passed-in
+     * Cell that we want to look for an adjacent Cell.
+     * @return - The adjacent cell.
+     */
+    public Cell getCellAdjacentTo(Cell cell, Direction direction) {
         for (Cell aCell : allCells) {
             if(aCell.X == direction.getX(cell) && aCell.Y == direction.getY(cell)) {
                 return cell;
@@ -28,4 +40,45 @@ public class Map {
         }
         return defaultCell;
     }
+    
+    /**
+     * This method determines the potential Entity that could exist in a Cell,
+     * given its surroundings.
+     * @param cell - The Cell we are querying.
+     * @return - The potential Entity that could exist in the Cell.
+     */
+    public Entity getPotentialEntity(Cell cell) {
+        // Check for rooms adjacent horizontally.
+        Cell westCell = getCellAdjacentTo(cell, Direction.WEST);
+        Cell eastCell = getCellAdjacentTo(cell, Direction.EAST);
+        if(westCell.isRoom() && eastCell.isRoom()) {
+            return Entity.HORIZONTAL_EXIT;
+        }
+        // Check for rooms adjacent vertically.
+        Cell northCell = getCellAdjacentTo(cell, Direction.NORTH);
+        Cell southCell = getCellAdjacentTo(cell, Direction.SOUTH);
+        if(northCell.isRoom() && southCell.isRoom()) {
+            return Entity.VERTICAL_EXIT;
+        }
+        // Check for rooms adjacent diagonally.
+        Cell northwestCell = getCellAdjacentTo(cell, Direction.NORTHWEST);
+        Cell northeastCell = getCellAdjacentTo(cell, Direction.NORTHEAST);
+        Cell southwestCell = getCellAdjacentTo(cell, Direction.SOUTHWEST);
+        Cell southeastCell = getCellAdjacentTo(cell, Direction.SOUTHEAST);
+        // Check for rooms across both diagonal axis.
+        if(northwestCell.isRoom() && northeastCell.isRoom() && 
+                southwestCell.isRoom() && southeastCell.isRoom()) {
+            return Entity.X_EXIT;
+        } 
+        // Check for rooms on the southwest/northeast axis.
+        else if (southwestCell.isRoom() && northeastCell.isRoom()) {
+            return Entity.FORWARD_DIAGONAL_EXIT;
+        }
+        // Check for rooms on the southeast/northwest axis.
+        else if (southeastCell.isRoom() && northwestCell.isRoom()) {
+            return Entity.BACKWARD_DIAGONAL_EXIT;
+        }
+    return Entity.NO_ENTITY;
+    }
+
 }
