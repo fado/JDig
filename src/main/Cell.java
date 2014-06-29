a445ca25cde3d0ff3d80daffab259a57ec206a0c
fado <pdonnelly19@qub.ac.uk>
2014-06-29 21:31:22 +0100

    Added methods to determine whether or not the Entity contained within the Cell is a room, and whether or not it lies within valid coordinate boundaries.

--- src/main/Cell.java
+++ src/main/Cell.java
@@ -4,9 +4,22 @@ public class Cell {
     
     public final int X;
     public final int Y;
+    private Entity currentEntity;
     
     public Cell(int column, int row) {
         this.X = column;
         this.Y = row;
+    }
+    
+    public void setEntity(Entity entity) {
+        this.currentEntity = entity;
+    }
+    
+    public boolean isRoom() {
+        return currentEntity.equals(Entity.ROOM) && this.isInBounds();
+    }
+    
+    private boolean isInBounds() {
+        return this.X >= 0 && this.Y >= 0;
     }
 }
