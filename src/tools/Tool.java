70fb34b5ea904f11f66e4d3255453a584c54c7f1
fado <pdonnelly19@qub.ac.uk>
2014-06-30 14:17:05 +0100

    Changed method signature to reflect changes to class names.

--- src/tools/Tool.java
+++ src/tools/Tool.java
@@ -4,7 +4,7 @@ import gui.CellPanel;
 import java.awt.event.MouseEvent;
 
 public interface Tool {
-    public void mouseEntered(CellPanel square, MouseEvent event);
-    public void mouseExited(CellPanel square, MouseEvent event);
-    public void mouseClicked(CellPanel square, MouseEvent event);
+    public void mouseEntered(CellPanel cell, MouseEvent event);
+    public void mouseExited(CellPanel cell, MouseEvent event);
+    public void mouseClicked(CellPanel cell, MouseEvent event);
 }
