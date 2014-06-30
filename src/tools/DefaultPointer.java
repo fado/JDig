056fb9e3a7f06e35816c07420f5a895fb97cb104
fado <pdonnelly19@qub.ac.uk>
2014-06-30 09:08:39 +0100

    Moved during package restructuring.

---
+++ src/tools/DefaultPointer.java
@@ -0,0 +1,23 @@
+package tools;
+
+import gui.SquareUI;
+import java.awt.event.MouseEvent;
+
+public class DefaultPointer implements Tool {
+
+    @Override
+    public void mouseEntered(SquareUI cell, MouseEvent event) {
+        
+    }
+
+    @Override
+    public void mouseExited(SquareUI cell, MouseEvent event) {
+        
+    }
+
+    @Override
+    public void mousePressed(SquareUI cell, MouseEvent event) {
+        
+    }
+    
+}
