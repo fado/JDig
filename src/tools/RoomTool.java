4f8527835cdc2119d39aaa76c7bb306c1db9338b
fado <pdonnelly19@qub.ac.uk>
2014-06-30 15:52:30 +0100

    Re-establishing earlier behaviours under the new paradigm.

--- src/tools/RoomTool.java
+++ src/tools/RoomTool.java
@@ -2,22 +2,41 @@ package tools;
 
 import gui.CellPanel;
 import java.awt.event.MouseEvent;
+import javax.swing.SwingUtilities;
+import main.Cell;
+import main.Entity;
+import main.Map;
 
 public class RoomTool implements Tool {
 
-    
     @Override
-    public void mouseEntered(CellPanel cell, MouseEvent event) {
-
+    public void mouseEntered(CellPanel cellPanel, MouseEvent event) {
+        Cell cellObject = cellPanel.getCellObject();
+        Map parentMap = cellObject.getParentMap();
+        if(parentMap.getPotentialEntity(cellObject).equals(Entity.NO_ENTITY)) {
+            cellPanel.addImage(Entity.ROOM.getPath());
+            cellPanel.removeBorder();
+        }
+        
     }
 
     @Override
-    public void mouseExited(CellPanel cell, MouseEvent event) {
-
+    public void mouseExited(CellPanel cellPanel, MouseEvent event) {
+        Cell cellObject = cellPanel.getCellObject();
+        if(!cellObject.isFilled()) {
+            cellPanel.removeImage();
+            cellPanel.restoreDefaultBorder();
+        }
     }
 
     @Override
-    public void mouseClicked(CellPanel cell, MouseEvent event) {
-
+    public void mouseClicked(CellPanel cellPanel, MouseEvent event) {
+        Cell cellObject = cellPanel.getCellObject();
+        if(SwingUtilities.isRightMouseButton(event)) {
+            cellObject.setEntity(Entity.NO_ENTITY);
+        } else if (SwingUtilities.isLeftMouseButton(event)) {
+            cellObject.setEntity(Entity.ROOM);
+            cellPanel.removeBorder();
+        }
     }
 }
