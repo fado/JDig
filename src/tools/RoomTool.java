0238aacc0b2f50988d3d8a970bf44c6bb5ec46ba
fado <pdonnelly19@qub.ac.uk>
2014-06-30 09:10:59 +0100

    Moved during package restructuring and removed code that will no longer work due to the new architecture changes.

---
+++ src/tools/RoomTool.java
@@ -0,0 +1,23 @@
+package tools;
+
+import gui.SquareUI;
+import java.awt.event.MouseEvent;
+
+public class RoomTool implements Tool {
+
+    
+    @Override
+    public void mouseEntered(SquareUI square, MouseEvent event) {
+
+    }
+
+    @Override
+    public void mouseExited(SquareUI square, MouseEvent event) {
+
+    }
+
+    @Override
+    public void mousePressed(SquareUI square, MouseEvent event) {
+
+    }
+}
