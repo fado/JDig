e31055b47d2ca756b4053e789bb0a71bd604351b
fado <pdonnelly19@qub.ac.uk>
2014-06-30 15:51:52 +0100

    Added paths to corresponding image files.

--- src/main/Entity.java
+++ src/main/Entity.java
@@ -1,11 +1,21 @@
 package main;
 
 public enum Entity {
-    ROOM,
-    HORIZONTAL_EXIT,
-    VERTICAL_EXIT,
-    FORWARD_DIAGONAL_EXIT,
-    BACKWARD_DIAGONAL_EXIT,
-    X_EXIT,
-    NO_ENTITY;
+    ROOM("./resources/images/room.png"),
+    HORIZONTAL_EXIT("./resources/images/horizontal_exit.png"),
+    VERTICAL_EXIT("./resources/images/vertical_exit.png"),
+    FORWARD_DIAGONAL_EXIT("./resources/images/forward_diagonal_exit.png"),
+    BACKWARD_DIAGONAL_EXIT("./resources/images/backward_diagonal_exit.png"),
+    X_EXIT("./resources/images/x_exit.png"),
+    NO_ENTITY(null);
+    
+    private final String path;
+    
+    Entity(String path) {
+        this.path = path;
+    }
+    
+    public String getPath() {
+        return this.path;
+    }
 }
