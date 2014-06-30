9fc54271e9e949e70b20400cb4d634b59541b5b9
fado <pdonnelly19@qub.ac.uk>
2014-06-30 09:11:20 +0100

    Moved during package restructuring.

---
+++ src/tools/ToolEvent.java
@@ -0,0 +1,17 @@
+package tools;
+
+import tools.Tool;
+import java.util.EventObject;
+
+public class ToolEvent extends EventObject {
+    private Tool tool;
+    
+    public ToolEvent(Object source, Tool tool) {
+        super(source);
+        this.tool = tool;
+    }
+    
+    public Tool getTool() {
+        return tool;
+    }
+}
