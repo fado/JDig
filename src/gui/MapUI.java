4a9cab11f1f5ab4b45c6d0f0ab637f08c62a56d9
fado <pdonnelly19@qub.ac.uk>
2014-06-30 09:14:38 +0100

    Removed vast swathes of logic code that now exist in Map.  Also renamed the class for clarity.  Currently broken until I figure out where the grid size is being passed in from.

---
+++ src/gui/MapUI.java
@@ -0,0 +1,37 @@
+package gui;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JPanel;
+
+/**
+ * The MapGrid class specifies a collection of MapSquare that comprise the user
+ * interface for drawing maps within JDig.
+ */
+public class MapUI extends JPanel {
+    
+    private List<SquareUI> squares;
+
+    public MapUI(int maxRows, int maxColumns) {
+        setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        if (squares == null) {
+            squares = new ArrayList<>();
+        }
+
+        for (int row = 0; row < maxRows; row++) {
+            for (int column = 0; column < maxColumns; column++) {
+                constraints.gridx = column;
+                constraints.gridy = row;
+
+                SquareUI square = new SquareUI(this, row, column);
+                squares.add(square);
+                add(square, constraints);
+            }
+        }
+    }
+    
+}
