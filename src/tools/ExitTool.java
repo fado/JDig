cb020d097a73742d6b89daec99670e79914fea2f
fado <pdonnelly19@qub.ac.uk>
2014-06-30 15:52:35 +0100

    Re-establishing earlier behaviours under the new paradigm.

--- src/tools/ExitTool.java
+++ src/tools/ExitTool.java
@@ -2,21 +2,40 @@ package tools;
 
 import gui.CellPanel;
 import java.awt.event.MouseEvent;
+import main.Cell;
+import main.Entity;
+import main.Map;
 
 public class ExitTool implements Tool {
     
     @Override
-    public void mouseEntered(CellPanel cell, MouseEvent event) {
-
+    public void mouseEntered(CellPanel cellPanel, MouseEvent event) {
+        Cell cellObject = cellPanel.getCellObject();
+        Map parentMap = cellObject.getParentMap();
+        Entity suggestedEntity = parentMap.getPotentialEntity(cellObject);
+        System.out.println(suggestedEntity);
+        if(!cellObject.isFilled() && !suggestedEntity.equals(Entity.NO_ENTITY)) {
+            cellPanel.addImage(suggestedEntity.getPath());
+        }
+        if (suggestedEntity.equals(Entity.VERTICAL_EXIT)) {
+            cellPanel.setVerticalExitBorder();
+        }
+        if (suggestedEntity.equals(Entity.HORIZONTAL_EXIT)) {
+            cellPanel.setHorizontalExitBorder();
+        }
     }
     
     @Override
-    public void mouseExited(CellPanel cell, MouseEvent event) {
-    
+    public void mouseExited(CellPanel cellPanel, MouseEvent event) {
+        Cell cellObject = cellPanel.getCellObject();
+        if(!cellObject.isFilled()) {
+            cellPanel.removeImage();
+            cellPanel.restoreDefaultBorder();
+        }
     }
 
     @Override
-    public void mouseClicked(CellPanel cell, MouseEvent event) {
+    public void mouseClicked(CellPanel cellPanel, MouseEvent event) {
  
     }
 }
