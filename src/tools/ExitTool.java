4413e3935bcea59109fd4ff19d7185cf27b02723
fado <pdonnelly19@qub.ac.uk>
2014-06-30 09:09:55 +0100

    Moved during package restructuring and removed code that now lives in Map.

---
+++ src/tools/ExitTool.java
@@ -0,0 +1,22 @@
+package tools;
+
+import gui.SquareUI;
+import java.awt.event.MouseEvent;
+
+public class ExitTool implements Tool {
+    
+    @Override
+    public void mouseEntered(SquareUI square, MouseEvent event) {
+
+    }
+    
+    @Override
+    public void mouseExited(SquareUI square, MouseEvent event) {
+    
+    }
+
+    @Override
+    public void mousePressed(SquareUI square, MouseEvent event) {
+ 
+    }
+}
