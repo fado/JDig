ae25c5920251f04938930492f97be0c8c12a425e
fado <pdonnelly19@qub.ac.uk>
2014-07-01 17:15:20 +0100

    Moved construction code to new class.

--- src/main/Jdig.java
+++ src/main/Jdig.java
@@ -1,29 +1,15 @@
 package main;
 
-import gui.MainUI;
-import java.io.IOException;
-
 public class Jdig {
     
-    private final int DEFAULT_ROWS = 40;
-    private final int DEFAULT_COLUMNS = 40;
-    private final Map currentMap;
-    
-    public static void main(String[] args) throws IOException {
-        new Jdig();
+    private static MapLoader loader = new MapLoader();
+
+    public static void main(String[] args) {
+        loader.newMap();
     }
     
-    private Jdig() {
-        currentMap = getCurrentMap();
-        MainUI gui = new MainUI(currentMap);
-        gui.run();
-    }
-    
-    private Map getCurrentMap() {
-        if(currentMap == null) {
-            return new Map(DEFAULT_COLUMNS, DEFAULT_ROWS);
-        }
-        return currentMap;
+    static void setMapLoader(MapLoader loader) {
+        Jdig.loader = loader;
     }
     
 }
