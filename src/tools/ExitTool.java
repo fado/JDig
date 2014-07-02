dfb5f5274358371a930dd3aa9c9b43e937ac0424
fado <pdonnelly19@qub.ac.uk>
2014-07-02 16:16:06 +0100

    Rewrote code to take into account new methods introduced in the CellPanel to provide access to the corresponding Cell.

--- src/tools/ExitTool.java
+++ src/tools/ExitTool.java
@@ -2,33 +2,28 @@ package tools;
 
 import gui.CellPanel;
 import java.awt.event.MouseEvent;
-import main.Cell;
 import main.Entity;
-import main.Map;
 
 public class ExitTool implements Tool {
     
     @Override
     public void mouseEntered(CellPanel cellPanel, MouseEvent event) {
-        Cell cellObject = cellPanel.getCellObject();
-        Map parentMap = cellObject.getParentMap();
-        Entity suggestedEntity = parentMap.getPotentialEntity(cellObject);
-        System.out.println(suggestedEntity);
-        if(!cellObject.isFilled() && !suggestedEntity.equals(Entity.NO_ENTITY)) {
-            cellPanel.addImage(suggestedEntity.getPath());
-        }
-        if (suggestedEntity.equals(Entity.VERTICAL_EXIT)) {
-            cellPanel.setVerticalExitBorder();
-        }
-        if (suggestedEntity.equals(Entity.HORIZONTAL_EXIT)) {
-            cellPanel.setHorizontalExitBorder();
+        if(!cellPanel.isFilled()) {
+            Entity potentialEntity = cellPanel.getPotentialEntity();
+            if(!potentialEntity.equals(Entity.NO_ENTITY)) {
+                cellPanel.addImage(cellPanel.getPotentialEntity().getPath());
+                if(potentialEntity.equals(Entity.VERTICAL_EXIT)) {
+                    cellPanel.setVerticalExitBorder();
+                } else if(potentialEntity.equals(Entity.HORIZONTAL_EXIT)) {
+                    cellPanel.setHorizontalExitBorder();
+                }
+            }
         }
     }
     
     @Override
     public void mouseExited(CellPanel cellPanel, MouseEvent event) {
-        Cell cellObject = cellPanel.getCellObject();
-        if(!cellObject.isFilled()) {
+        if(!cellPanel.isFilled()) {
             cellPanel.removeImage();
             cellPanel.restoreDefaultBorder();
         }
