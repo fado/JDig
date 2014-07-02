929febe918fae542b827ce0880db72110965b92f
fado <pdonnelly19@qub.ac.uk>
2014-07-02 17:06:15 +0100

    Moved the code to calculate potential entities for a Cell to the Cell class.

--- src/main/Map.java
+++ src/main/Map.java
@@ -72,45 +72,5 @@ public class Map {
         }
         return defaultCell;
     }
-    
-    /**
-     * Determines the potential Entity that could exist in a Cell, given its surroundings.
-     * 
-     * @param cell - The Cell we are querying.
-     * @return - The potential Entity that could exist in the Cell.
-     */
-    public Entity getPotentialEntity(Cell cell) {
-        // Check for rooms adjacent horizontally.
-        Cell westCell = getCellAdjacentTo(cell, Direction.WEST);
-        Cell eastCell = getCellAdjacentTo(cell, Direction.EAST);
-        if(westCell.isRoom() && eastCell.isRoom()) {
-            return Entity.HORIZONTAL_EXIT;
-        }
-        // Check for rooms adjacent vertically.
-        Cell northCell = getCellAdjacentTo(cell, Direction.NORTH);
-        Cell southCell = getCellAdjacentTo(cell, Direction.SOUTH);
-        if(northCell.isRoom() && southCell.isRoom()) {
-            return Entity.VERTICAL_EXIT;
-        }
-        // Check for rooms adjacent diagonally.
-        Cell northwestCell = getCellAdjacentTo(cell, Direction.NORTHWEST);
-        Cell northeastCell = getCellAdjacentTo(cell, Direction.NORTHEAST);
-        Cell southwestCell = getCellAdjacentTo(cell, Direction.SOUTHWEST);
-        Cell southeastCell = getCellAdjacentTo(cell, Direction.SOUTHEAST);
-        // Check for rooms across both diagonal axis.
-        if(northwestCell.isRoom() && northeastCell.isRoom() && 
-                southwestCell.isRoom() && southeastCell.isRoom()) {
-            return Entity.X_EXIT;
-        } 
-        // Check for rooms on the southwest/northeast axis.
-        else if (southwestCell.isRoom() && northeastCell.isRoom()) {
-            return Entity.FORWARD_DIAGONAL_EXIT;
-        }
-        // Check for rooms on the southeast/northwest axis.
-        else if (southeastCell.isRoom() && northwestCell.isRoom()) {
-            return Entity.BACKWARD_DIAGONAL_EXIT;
-        }
-    return Entity.NO_ENTITY;
-    }
 
 }
