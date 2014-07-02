1d284b376e698a63d1f44bfa930c3888afbd39fb
fado <pdonnelly19@qub.ac.uk>
2014-07-02 17:06:43 +0100

    Changed method signatures to include a Cell object and started referencing the Cell directly.

--- src/tools/ExitTool.java
+++ src/tools/ExitTool.java
@@ -2,16 +2,17 @@ package tools;
 
 import gui.CellPanel;
 import java.awt.event.MouseEvent;
+import main.Cell;
 import main.Entity;
 
 public class ExitTool implements Tool {
     
     @Override
-    public void mouseEntered(CellPanel cellPanel, MouseEvent event) {
-        if(!cellPanel.isFilled()) {
-            Entity potentialEntity = cellPanel.getPotentialEntity();
+    public void mouseEntered(CellPanel cellPanel, Cell cell, MouseEvent event) {
+        if(!cell.isFilled()) {
+            Entity potentialEntity = cell.getPotentialEntity();
             if(!potentialEntity.equals(Entity.NO_ENTITY)) {
-                cellPanel.addImage(cellPanel.getPotentialEntity().getPath());
+                cellPanel.addImage(potentialEntity.getPath());
                 if(potentialEntity.equals(Entity.VERTICAL_EXIT)) {
                     cellPanel.setVerticalExitBorder();
                 } else if(potentialEntity.equals(Entity.HORIZONTAL_EXIT)) {
@@ -22,15 +23,15 @@ public class ExitTool implements Tool {
     }
     
     @Override
-    public void mouseExited(CellPanel cellPanel, MouseEvent event) {
-        if(!cellPanel.isFilled()) {
+    public void mouseExited(CellPanel cellPanel, Cell cell, MouseEvent event) {
+        if(!cell.isFilled()) {
             cellPanel.removeImage();
             cellPanel.restoreDefaultBorder();
         }
     }
 
     @Override
-    public void mouseClicked(CellPanel cellPanel, MouseEvent event) {
+    public void mouseClicked(CellPanel cellPanel, Cell cell, MouseEvent event) {
  
     }
 }
