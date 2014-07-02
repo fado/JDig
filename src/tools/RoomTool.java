729c8cc544bb48a3ae83d7b57a051f3300b84256
fado <pdonnelly19@qub.ac.uk>
2014-07-02 17:06:47 +0100

    Changed method signatures to include a Cell object and started referencing the Cell directly.

--- src/tools/RoomTool.java
+++ src/tools/RoomTool.java
@@ -3,32 +3,33 @@ package tools;
 import gui.CellPanel;
 import java.awt.event.MouseEvent;
 import javax.swing.SwingUtilities;
+import main.Cell;
 import main.Entity;
 
 public class RoomTool implements Tool {
 
     @Override
-    public void mouseEntered(CellPanel cellPanel, MouseEvent event) {
-        if (!cellPanel.isFilled()) {
+    public void mouseEntered(CellPanel cellPanel, Cell cell, MouseEvent event) {
+        if (!cell.isFilled()) {
             cellPanel.addImage(Entity.ROOM.getPath());
             cellPanel.removeBorder();
         }
     }
 
     @Override
-    public void mouseExited(CellPanel cellPanel, MouseEvent event) {
-        if (!cellPanel.isFilled()) {
+    public void mouseExited(CellPanel cellPanel, Cell cell, MouseEvent event) {
+        if (!cell.isFilled()) {
             cellPanel.removeImage();
             cellPanel.restoreDefaultBorder();
         }
     }
 
     @Override
-    public void mouseClicked(CellPanel cellPanel, MouseEvent event) {
+    public void mouseClicked(CellPanel cellPanel, Cell cell, MouseEvent event) {
         if (SwingUtilities.isRightMouseButton(event)) {
-            cellPanel.setEntity(Entity.NO_ENTITY);
+            cell.setEntity(Entity.NO_ENTITY);
         } else if (SwingUtilities.isLeftMouseButton(event)) {
-            cellPanel.setEntity(Entity.ROOM);
+            cell.setEntity(Entity.ROOM);
         }
     }
 }
