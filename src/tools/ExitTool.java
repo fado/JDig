fa2ef53c3a2eddec27cbf5e944378794d5f05814
fado <pdonnelly19@qub.ac.uk>
2014-07-04 10:27:55 +0100

    Can now add exits to the Map.  Or rather, can draw the map entity onto the CellPanel.

--- src/tools/ExitTool.java
+++ src/tools/ExitTool.java
@@ -2,15 +2,18 @@ package tools;
 
 import gui.CellPanel;
 import java.awt.event.MouseEvent;
+import javax.swing.SwingUtilities;
 import main.Cell;
 import main.Entity;
 
 public class ExitTool implements Tool {
     
+    Entity potentialEntity;
+    
     @Override
     public void mouseEntered(Cell cell, MouseEvent event) {
         CellPanel cellPanel = (CellPanel)event.getSource();
-        Entity potentialEntity = cell.getPotentialEntity();
+        potentialEntity = cell.getPotentialEntity();
         
         if(!cell.isFilled()) {    
             if(!potentialEntity.equals(Entity.NO_ENTITY)) {
@@ -22,7 +25,11 @@ public class ExitTool implements Tool {
 
     @Override
     public void mouseClicked(Cell cell, MouseEvent event) {
- 
+        if (SwingUtilities.isRightMouseButton(event)) {
+            cell.setEntity(Entity.NO_ENTITY);
+        } else if (SwingUtilities.isLeftMouseButton(event)) {
+            cell.setEntity(potentialEntity);
+        }
     }
  
     private void setBorder(CellPanel cellPanel, Entity potentialEntity) {
