8b2f841204d19f933c51915311a183826d720eb1
fado <pdonnelly19@qub.ac.uk>
2014-07-04 12:06:53 +0100

    Added Javadocs for new methods.

--- src/main/Room.java
+++ src/main/Room.java
@@ -8,59 +8,74 @@ import java.util.Map;
  * This class specifies a room object within the Jdig application.
  */
 public class Room {
-   
+
     private final Point position;
     private final Map<String, Room> exits;
     private String name;
-    
+
     /**
      * Create a new Room object with the passed-in parameter.
-     * @param point 
+     *
+     * @param point
      */
     public Room(Point point) {
         this.position = point;
         this.exits = new HashMap();
     }
-    
+
     /**
      * Returns the position of the room in the map.
+     *
      * @return - A Point representing the position of the room in the map.
      */
     public Point getPosition() {
         return this.position;
     }
-   
+
     /**
      * Adds an exit to this room.
+     *
      * @param direction - The compass direction in which the exit leads.
      * @param destination - The Room to which the exit leads.
      */
     public void addExit(Direction direction, Room destination) {
         exits.put(direction.toString(), destination);
     }
-   
+
     /**
      * Returns a list of exits leading out of the room.
+     *
      * @return - A List of exits leading out of the room.
      */
     public Map<String, Room> getExits() {
         return this.exits;
     }
-   
+
+    /**
+     * Sets the name of the Room.
+     * 
+     * @param name - The name to be set.
+     */
     public void setName(String name) {
         this.name = name;
     }
-    
+
+    /**
+     * Gets the name of the room.
+     * 
+     * @return - The name of the room.
+     */
     public String getName() {
         return this.name;
     }
-    
+
     /**
      * Overridden toString() method.
+     *
      * @return - A String containing the x and y map coordinates of the room.
      */
     @Override
     public String toString() {
-        return position.x +", "+position.y;
+        return position.x + ", " + position.y;
     }
 }
