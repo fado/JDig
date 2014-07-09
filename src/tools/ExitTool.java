c045756258fbd9358ee5ebc88a747e66bb7dca62
fado <pdonnelly19@qub.ac.uk>
2014-07-09 10:04:10 +0100

    Made changes for code clarity and reintroduced some code that had been moved up to CellPanel, even though it will mean a little bit of duplication of code.  Realised with a fresh pair of eyes that I was moving business logic back into the UI, even though it was only slightly.

--- src/tools/ExitTool.java
+++ src/tools/ExitTool.java
@@ -4,38 +4,48 @@ import gui.CellPanel;
 import java.awt.event.MouseEvent;
 import javax.swing.SwingUtilities;
 import main.Cell;
-import main.Entity;
+import main.entities.EntityType;
+import main.entities.ExitType;
 
 public class ExitTool implements Tool {
     
-    Entity potentialEntity;
+    ExitType exitType;
     
     @Override
     public void mouseEntered(Cell cell, MouseEvent event) {
         CellPanel cellPanel = (CellPanel)event.getSource();
-        potentialEntity = cell.getPotentialEntity();
+        exitType = cell.getPotentialExitType();
         
-        if(!cell.isFilled()) {    
-            if(!potentialEntity.equals(Entity.NO_ENTITY)) {
-                cellPanel.addImage(potentialEntity.getPath());
-                setBorder(cellPanel, potentialEntity);
+        if(!cell.isFilled()) {
+            if(exitType != null) {
+                cellPanel.addImage(exitType.getPath());
+                setBorder(cellPanel, exitType);    
             }
+        }
+    }
+
+    @Override
+    public void mouseExited(Cell cell, MouseEvent event) {
+        CellPanel cellPanel = (CellPanel)event.getSource();
+        if (!cell.isFilled()) {
+            cellPanel.removeImage();
+            cellPanel.restoreDefaultBorder();
         }
     }
 
     @Override
     public void mouseClicked(Cell cell, MouseEvent event) {
         if (SwingUtilities.isRightMouseButton(event)) {
-            cell.setEntity(Entity.NO_ENTITY);
+            cell.setEntityType(EntityType.NO_ENTITY);
         } else if (SwingUtilities.isLeftMouseButton(event)) {
-            cell.setEntity(potentialEntity);
+            cell.setFilled(true);
         }
     }
  
-    private void setBorder(CellPanel cellPanel, Entity potentialEntity) {
-        if (potentialEntity.equals(Entity.VERTICAL_EXIT)) {
+    private void setBorder(CellPanel cellPanel, ExitType exitType) {
+        if (exitType == ExitType.VERTICAL_EXIT) {
             cellPanel.setVerticalExitBorder();
-        } else if (potentialEntity.equals(Entity.HORIZONTAL_EXIT)) {
+        } else if (exitType == ExitType.HORIZONTAL_EXIT) {            
             cellPanel.setHorizontalExitBorder();
         }
     }
