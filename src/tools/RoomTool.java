5bfb0e01c40784f9220155c6a84413dfaacd7bb5
fado <pdonnelly19@qub.ac.uk>
2014-07-09 10:04:15 +0100

    Made changes for code clarity and reintroduced some code that had been moved up to CellPanel, even though it will mean a little bit of duplication of code.  Realised with a fresh pair of eyes that I was moving business logic back into the UI, even though it was only slightly.

--- src/tools/RoomTool.java
+++ src/tools/RoomTool.java
@@ -4,25 +4,36 @@ import gui.CellPanel;
 import java.awt.event.MouseEvent;
 import javax.swing.SwingUtilities;
 import main.Cell;
-import main.Entity;
+import main.entities.EntityType;
 
 public class RoomTool implements Tool {
 
+    private final String ROOM_IMAGE = "./resources/images/room.png";
+    
     @Override
     public void mouseEntered(Cell cell, MouseEvent event) {
         CellPanel cellPanel = (CellPanel)event.getSource();
         if (!cell.isFilled()) {
-            cellPanel.addImage(Entity.ROOM.getPath());
+            cellPanel.addImage(ROOM_IMAGE);
             cellPanel.removeBorder();
+        }
+    }
+    
+    @Override
+    public void mouseExited(Cell cell, MouseEvent event) {
+        CellPanel cellPanel = (CellPanel) event.getSource();
+        if (!cell.isFilled()) {
+            cellPanel.removeImage();
+            cellPanel.restoreDefaultBorder();
         }
     }
 
     @Override
     public void mouseClicked(Cell cell, MouseEvent event) {
         if (SwingUtilities.isRightMouseButton(event)) {
-            cell.setEntity(Entity.NO_ENTITY);
+            cell.setEntityType(EntityType.NO_ENTITY);
         } else if (SwingUtilities.isLeftMouseButton(event)) {
-            cell.setEntity(Entity.ROOM);
+            cell.setEntityType(EntityType.ROOM);
         }
     }
 }
