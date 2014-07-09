bfe9d68262b95b35662face75a059495fc2956a6
fado <pdonnelly19@qub.ac.uk>
2014-07-09 10:00:03 +0100

    Started adding the code to build the UI.

--- src/gui/AttributesPanel.java
+++ src/gui/AttributesPanel.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.awt.Dimension;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import net.miginfocom.swing.MigLayout;
 
 public class AttributesPanel extends JPanel {
@@ -9,6 +10,12 @@ public class AttributesPanel extends JPanel {
     public AttributesPanel() {
         setLayout(new MigLayout());
         setPreferredSize(new Dimension(400,400));
+        
+        JTextField roomNameField = new JTextField(10);
+        roomNameField.setActionCommand("Room name...");
+        
+        this.add(roomNameField);
+        
     }
     
 }
