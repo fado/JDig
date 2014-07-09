be9761035b3e808ddb2ef08fd8c225ba1d352c4a
fado <pdonnelly19@qub.ac.uk>
2014-07-09 10:02:50 +0100

    Paired back the Room code to the bare minimum for now.

---
+++ src/main/entities/Room.java
@@ -0,0 +1,15 @@
+package main.entities;
+
+public class Room {
+
+    private String name;
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+       return this.name;
+    }
+    
+}
