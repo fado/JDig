e263dd07236256e6160a8b73908a2834edd79cad
fado <pdonnelly19@qub.ac.uk>
2014-07-10 15:22:56 +0100

    Started adding UI components to the attributes side of the GUI.

--- src/gui/AttributesPanel.java
+++ src/gui/AttributesPanel.java
@@ -1,6 +1,11 @@
 package gui;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import net.miginfocom.swing.MigLayout;
@@ -11,11 +16,29 @@ public class AttributesPanel extends JPanel {
         setLayout(new MigLayout());
         setPreferredSize(new Dimension(400,400));
         
-        JTextField roomNameField = new JTextField(10);
-        roomNameField.setActionCommand("Room name...");
+        JPanel contentPanel = new JPanel();
+        contentPanel.setLayout(new MigLayout());
+        contentPanel.setOpaque(true);
+        //contentPanel.setBackground(Color.WHITE);
+        contentPanel.setBorder(
+                BorderFactory.createTitledBorder("Attributes"));
         
-        this.add(roomNameField);
+        JTextField roomNameField = new JTextField(20);
+        JComboBox streetNameField = new JComboBox();
+        JButton addEditStreetsButton = new JButton("Add/Edit Streets");
         
+        JLabel roomNameLabel = new JLabel("Room name:", JLabel.RIGHT);
+        JLabel streetNameLabel = new JLabel("Street name:", JLabel.RIGHT);
+        roomNameLabel.setLabelFor(roomNameField);
+        streetNameLabel.setLabelFor(streetNameField);
+        
+        contentPanel.add(roomNameLabel);
+        contentPanel.add(roomNameField, "wrap");
+        contentPanel.add(streetNameLabel);
+        contentPanel.add(streetNameField);
+        contentPanel.add(addEditStreetsButton);
+        
+        this.add(contentPanel);
     }
     
 }
