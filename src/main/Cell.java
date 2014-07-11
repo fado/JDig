5ef0498c767867b37eb04a847eaf1a3550af37f9
fado <pdonnelly19@qub.ac.uk>
2014-07-11 21:03:58 +0100

    Factored the code to find adjacent rooms out of the method to find potential entities.  Also made changes to reflect the renaming of the Level class.

--- src/main/Cell.java
+++ src/main/Cell.java
@@ -9,19 +9,21 @@ public class Cell {
 
     public final int X;
     public final int Y;
-    private final Map parentMap;
+    private final Level parentMap;
     private EntityType currentEntity;
     private boolean filled;
     private Room room;
+    private Cell northCell, southCell, eastCell, westCell, northwestCell,
+            northeastCell, southwestCell, southeastCell;
 
-    public Cell(Point point, Map map) {
+    public Cell(Point point, Level map) {
         this.X = point.x;
         this.Y = point.y;
         this.parentMap = map;
         this.currentEntity = EntityType.NO_ENTITY;
     }
 
-    public Map getParentMap() {
+    public Level getParentMap() {
         return this.parentMap;
     }
 
@@ -68,34 +70,40 @@ public class Cell {
      * @return - The potential Entity that could exist in the Cell.
      */
     public ExitType getPotentialExitType() {
+        getAdjacentRooms();
         // Check for rooms adjacent horizontally.
-        Cell westCell = parentMap.getCellAdjacentTo(this, Direction.WEST);
-        Cell eastCell = parentMap.getCellAdjacentTo(this, Direction.EAST);
         if (westCell.isRoom() && eastCell.isRoom()) {
             return ExitType.HORIZONTAL_EXIT;
         }
         // Check for rooms adjacent vertically.
-        Cell northCell = parentMap.getCellAdjacentTo(this, Direction.NORTH);
-        Cell southCell = parentMap.getCellAdjacentTo(this, Direction.SOUTH);
         if (northCell.isRoom() && southCell.isRoom()) {
             return ExitType.VERTICAL_EXIT;
         }
-        // Check for rooms adjacent diagonally.
-        Cell northwestCell = parentMap.getCellAdjacentTo(this, Direction.NORTHWEST);
-        Cell northeastCell = parentMap.getCellAdjacentTo(this, Direction.NORTHEAST);
-        Cell southwestCell = parentMap.getCellAdjacentTo(this, Direction.SOUTHWEST);
-        Cell southeastCell = parentMap.getCellAdjacentTo(this, Direction.SOUTHEAST);
         // Check for rooms across both diagonal axis.
         if (northwestCell.isRoom() && northeastCell.isRoom()
                 && southwestCell.isRoom() && southeastCell.isRoom()) {
             return ExitType.X_EXIT;
-        } // Check for rooms on the southwest/northeast axis.
+        } 
+        // Check for rooms on the southwest/northeast axis.
         else if (southwestCell.isRoom() && northeastCell.isRoom()) {
             return ExitType.FORWARD_DIAGONAL_EXIT;
-        } // Check for rooms on the southeast/northwest axis.
+        } 
+        // Check for rooms on the southeast/northwest axis.
         else if (southeastCell.isRoom() && northwestCell.isRoom()) {
             return ExitType.BACKWARD_DIAGONAL_EXIT;
         }
         return null;
     }
+    
+    public void getAdjacentRooms() {
+        westCell = parentMap.getCellAdjacentTo(this, Direction.WEST);
+        eastCell = parentMap.getCellAdjacentTo(this, Direction.EAST);
+        northCell = parentMap.getCellAdjacentTo(this, Direction.NORTH);
+        southCell = parentMap.getCellAdjacentTo(this, Direction.SOUTH);
+        northwestCell = parentMap.getCellAdjacentTo(this, Direction.NORTHWEST);
+        northeastCell = parentMap.getCellAdjacentTo(this, Direction.NORTHEAST);
+        southwestCell = parentMap.getCellAdjacentTo(this, Direction.SOUTHWEST);
+        southeastCell = parentMap.getCellAdjacentTo(this, Direction.SOUTHEAST);
+    }
+    
 }
