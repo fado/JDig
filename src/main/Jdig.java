fd231909c68025236a9729a6b10f6ec41a205f79
fado <pdonnelly19@qub.ac.uk>
2014-07-11 21:05:21 +0100

    Updated to reflect renaming of Map class to Level.

--- src/main/Jdig.java
+++ src/main/Jdig.java
@@ -5,7 +5,7 @@ import gui.MainUI;
 public class Jdig {
 
     public static void main(String[] args) {
-        MainUI ui = new MainUI(new Map(40, 40));
+        MainUI ui = new MainUI(new Level(40, 40));
         ui.run();
     }
 }
