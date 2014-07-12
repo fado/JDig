c16fd7437e0b1d11006f6dfc70cddf26af85cc1e
fado <pdonnelly19@qub.ac.uk>
2014-07-12 18:52:20 +0100

    Incorrect path to back diagonal graphic.

--- src/main/entities/Exit.java
+++ src/main/entities/Exit.java
@@ -4,7 +4,7 @@ public enum Exit {
     HORIZONTAL_EXIT("./resources/images/horizontal_exit.png"),
     VERTICAL_EXIT("./resources/images/vertical_exit.png"),
     FORWARD_DIAGONAL_EXIT("./resources/images/forward_diagonal_exit.png"),
-    BACKWARD_DIAGONAL_EXIT("./resources/images/backward_diagonal_exit.png"),
+    BACKWARD_DIAGONAL_EXIT("./resources/images/back_diagonal_exit.png"),
     X_EXIT("./resources/images/x_exit.png");
     
     private final String path;
