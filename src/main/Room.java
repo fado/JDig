369148b9ca876ef65583c302d8bb0908aa1bad10
fado <pdonnelly19@qub.ac.uk>
2014-07-12 20:14:21 +0100

    Added list of exits to the Room, along with getters and setters.

--- src/main/Room.java
+++ src/main/Room.java
@@ -1,10 +1,11 @@
 package main;
 
-import java.util.Map;
+import java.util.List;
 
 public class Room {
     
     private String name;
+    private List<ExitModel> exits;
     
     public void setName(String name) {
         this.name = name;
@@ -12,6 +13,14 @@ public class Room {
 
     public String getName() {
        return this.name;
+    }
+    
+    public void addExit(ExitModel exit) {
+        exits.add(exit);
+    }
+    
+    public List<ExitModel> getExits() {
+        return this.exits;
     }
     
 }
