dcbe8c7cd556e148981ecf5e610a6cdaab75fc2d
fado <pdonnelly19@qub.ac.uk>
2014-07-13 19:33:01 +0100

    Implemented methods to clean up the construction of the MapToolbar.

--- src/gui/MapToolbar.java
+++ src/gui/MapToolbar.java
@@ -23,8 +23,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JToolBar;
 import tools.SelectionTool;
 import tools.ExitTool;
@@ -40,58 +38,32 @@ import tools.ToolListener;
  */
 public class MapToolbar extends JToolBar {
 
-    private final String IMAGES = "./resources/images/";
-    private final String SELECTION_TOOL_ICON = IMAGES + "selection_tool_16.png";
-    private final String ROOM_TOOL_ICON = IMAGES + "room_16.png";
-    private final String EXIT_TOOL_ICON = IMAGES + "exit_tool_16.png";
-    private final List<ToolListener> listeners;
+    private final List<ToolListener> listeners = new ArrayList<>();
     private Tool selectedTool;
     private Tool selectionTool;
+    private Tool roomTool;
+    private Tool exitTool;
     
     public MapToolbar() {
-        this.setFloatable(false);
-        this.listeners = new ArrayList<>();
-        // SelectionTool has to preserve some state, so let's just have one.
-        this.selectionTool = new SelectionTool();
-        this.selectedTool = selectionTool;
-
-        ImageIcon selectionToolIcon = new ImageIcon(SELECTION_TOOL_ICON);
-        ImageIcon roomToolIcon = new ImageIcon(ROOM_TOOL_ICON);
-        ImageIcon exitToolIcon = new ImageIcon(EXIT_TOOL_ICON);
-
-        final JButton selectionToolButton = new JButton(selectionToolIcon);
-        selectionToolButton.setToolTipText("Selection Tool");
-        final JButton roomToolButton = new JButton(roomToolIcon);
-        roomToolButton.setToolTipText(("Room Tool"));
-        final JButton exitToolButton = new JButton(exitToolIcon);
-        exitToolButton.setToolTipText("Exit Tool");
-
-        this.add(selectionToolButton);
-        this.add(roomToolButton);
-        this.add(exitToolButton);
+        setDefaultProperties();
+        setDefaultSelectionTool();
         
-        selectionToolButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent event) {
-                setSelectedTool(selectionTool);
-            }
-        });
-
-        roomToolButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent event) {
-                setSelectedTool(new RoomTool());
-            }
-        });
-
-        exitToolButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent event) {
-                setSelectedTool(new ExitTool());
-            }
-        });
+        this.add(ToolbarButton.build("SelectionTool", getToolChangeAction(selectionTool)));
+        this.add(ToolbarButton.build("RoomTool", getToolChangeAction(roomTool)));
+        this.add(ToolbarButton.build("ExitTool", getToolChangeAction(exitTool)));
     }
 
+    private void setDefaultProperties() {
+        this.setFloatable(false);
+    }
+    
+    private void setDefaultSelectionTool() {
+        this.selectionTool = new SelectionTool();
+        this.roomTool = new RoomTool();
+        this.exitTool = new ExitTool();
+        this.selectedTool = selectionTool;
+    }
+    
     public Tool getSelectedTool() {
         return this.selectedTool;
     }
@@ -99,6 +71,16 @@ public class MapToolbar extends JToolBar {
     public void setSelectedTool(Tool tool) {
         this.selectedTool = tool;
         fireToolChanged();
+    }
+    
+    private ActionListener getToolChangeAction(final Tool tool) {
+        ActionListener listener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                setSelectedTool(tool);
+            }
+        };
+        return listener;
     }
     
     public void addToolListener(ToolListener obj) {
