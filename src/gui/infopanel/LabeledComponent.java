8bd78a684e6f8603f023ae4df19d069746bbf287
fado <pdonnelly19@qub.ac.uk>
2014-07-18 20:59:26 +0100

    Added GPL.  Added ability to add JTextArea and set text in it.

--- src/gui/infopanel/LabeledComponent.java
+++ src/gui/infopanel/LabeledComponent.java
@@ -1,13 +1,33 @@
 package gui.infopanel;
 
+/**
+ * JDig, a tool for the automatic generation of LPC class files for Epitaph 
+ * developers.  Copyright (C) 2014 Fado@Epitaph.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.event.DocumentListener;
+import javax.swing.text.JTextComponent;
 
 public class LabeledComponent {
     
@@ -28,6 +48,12 @@ public class LabeledComponent {
         return new LabeledComponent(component, labelText);
     }
     
+    public static LabeledComponent textArea(String labelText, DocumentListener documentListener) {
+        JTextArea component = new JTextArea(5, 60);
+        component.getDocument().addDocumentListener(documentListener);
+        return new LabeledComponent(component, labelText);
+    }
+    
     public static LabeledComponent comboBox(String labelText, ActionListener actionListener) {
         JComboBox component = new JComboBox();
         component.addActionListener(actionListener);
@@ -45,7 +71,7 @@ public class LabeledComponent {
     }
     
     public void setText(String string) {
-        JTextField textField =(JTextField)this.component;
+        JTextComponent textField =(JTextComponent)this.component;
         textField.setText(string);
     }
     
@@ -53,6 +79,5 @@ public class LabeledComponent {
         panel.add(label);
         panel.add(component, "wrap");
     }
-    
-    
+
 }
