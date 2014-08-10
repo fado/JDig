b8762baa255db7e58c2cc7e81601fd733ab18163
fado <pdonnelly19@qub.ac.uk>
2014-08-10 13:58:51 +0100

    Factored the population of Cells within the level out into its own private method.  Also added complete Javadoc for the class.

--- src/data/Level.java
+++ src/data/Level.java
@@ -23,6 +23,10 @@ import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The Level class represents a collection of Cells that make up a single area.
+ * @author Padraig Donnelly
+ */
 public class Level {
 
     private final List<Cell> allCells;
@@ -32,28 +36,61 @@ public class Level {
     public Level(int maxColumns, int maxRows) {
         this.defaultCell = new Cell(new Point(-1, -1), this);
         this.allCells = new ArrayList<>();
+        populateLevel(maxColumns, maxRows);
+    }
 
-        for (int rows = 0; rows < maxColumns; rows++) {
-            for (int columns = 0; columns < maxRows; columns++) {
+    /**
+     * Populates the Level with Cells.
+     * @param maxColumns Maximum number of columns within the Level.
+     * @param maxRows Maximum number of Rows within the Level.
+     */
+    private void populateLevel(int maxColumns, int maxRows) {
+        for (int rows = 0; rows < maxRows; rows++) {
+            for (int columns = 0; columns < maxColumns; columns++) {
                 Cell cell = new Cell(new Point(columns, rows), this);
                 this.addCell(cell);
             }
         }
-
     }
 
+    /**
+     * Add a new Cell to the Level.
+     * @param cell The Cell to be added.
+     */
+    private void addCell(Cell cell) {
+        allCells.add(cell);
+    }
+
+    /**
+     * Add a Street to the Level.
+     * @param street The Street to be added.
+     */
     public void addStreet(Street street) {
         streets.add(street);
     }
 
+    /**
+     * Remove a Street from the Level.
+     * @param street The Street to be removed.
+     */
     public void removeStreet(Street street) {
         streets.remove(street);
     }
 
+    /**
+     * Get a List of Streets in the Level.
+     * @return a List of Streets in the Level.
+     */
     public List<Street> getStreets() {
         return this.streets;
     }
 
+    /**
+     * Get a single Street from the List of Streets contained within the Level.
+     * Matches the Street by its name.
+     * @param streetName The name of the Street to be retrieved.
+     * @return the Street whose name matches the one supplied.
+     */
     public Street getStreet(String streetName) {
         Street streetToBeReturned = null;
         for (Street street : streets) {
@@ -64,10 +101,11 @@ public class Level {
         return streetToBeReturned;
     }
 
-    private void addCell(Cell cell) {
-        allCells.add(cell);
-    }
-
+    /**
+     * Get a Cell at a specified point within the Level.
+     * @param point The point at which the Cell exists.
+     * @return the Cell that exits at the point supplied.
+     */
     public Cell getCellAt(Point point) {
         for (Cell cell : allCells) {
             if (cell.X == point.x && cell.Y == point.y) {
@@ -77,20 +115,36 @@ public class Level {
         return defaultCell;
     }
 
+    /**
+     * Get a list of all Cells within the Level.
+     * @return a list of all Cells within the Level.
+     */
     public List<Cell> getAllCells() {
         return this.allCells;
     }
 
+    /**
+     * Iterates through all Cells in the Level and returns those that contain
+     * Rooms.
+     * @return a list of all Cells within the Level that contain Rooms.
+     */
     public List<Room> getRooms() {
         List<Room> rooms = new ArrayList<>();
         for (Cell cell : allCells) {
-            if (cell.isRoom()) {
+            if (cell.hasRoom()) {
                 rooms.add(cell.getRoom());
             }
         }
         return rooms;
     }
 
+    /**
+     * Finds the Cell that lies adjacent to the pass-in Cell, in the passed-in
+     * Direction.
+     * @param referenceCell The Cell that is used as the reference point.
+     * @param direction The direction in which you want to look for an adjacent Cell.
+     * @return the Cell found.
+     */
     public Cell getCellAdjacentTo(Cell referenceCell, Direction direction) {
         for (Cell cell : allCells) {
             if (cell.X == direction.translateX(referenceCell) &&
