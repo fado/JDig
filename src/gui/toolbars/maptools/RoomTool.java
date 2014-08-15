5a077904f09afc89b273a929471215b903aa81c6
fado <pdonnelly19@qub.ac.uk>
2014-08-15 13:02:51 +0100

    Added JavaDoc and comments.  Cleaned up a lot of the code for better clarity.  Made changes so that the code now works with the refactored Model layer.

--- src/gui/toolbars/maptools/RoomTool.java
+++ src/gui/toolbars/maptools/RoomTool.java
@@ -20,116 +20,105 @@ package gui.toolbars.maptools;
  */
 
 import data.Cell;
-import data.Entity;
-import data.Exit;
 import data.Level;
 import data.Room;
 import gui.CellPanel;
+import properties.Images;
 
 import java.awt.event.MouseEvent;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import javax.swing.SwingUtilities;
 
+/**
+ * This class controls the addition and removal of Rooms from a Level.
+ */
 public class RoomTool implements MapTool {
 
-    private final String ROOM_IMAGE = "./resources/images/room.png";
-    private final String INVALID_ROOM_IMAGE = "./resources/images/invalid_room.png";
     private Cell firstRoomCell;
     private boolean xEven = false;
     private boolean yEven = false;
     private Level level;
+    private Images images = new Images();
 
+    /**
+     * Default constructor.
+     * @param level The Level on which the tool will be operating.
+     */
     public RoomTool(Level level) {
         this.level = level;
     }
-    
+
+    /**
+     * Controls what image should be displayed when the user mouses over the Cell.
+     * It performs two checks, firstly to ensure that no Entity already exists in
+     * the Cell.  If not, it checks whether or not a Room can be placed within the
+     * Cell.  This check is based upon the Cell position of the first Room to be
+     * added to the level.
+     * @param cell
+     * @param event The event that originated the method call.
+     */
     @Override
     public void mouseEntered(Cell cell, MouseEvent event) {
         CellPanel cellPanel = (CellPanel)event.getSource();
-        if (cell.getEntity().equals(Entity.NO_ENTITY)) {
+        // Check that the Cell does not already contain an Entity.
+        if (cellPanel.getCell().getEntity() == null) {
+            // Check whether or not the Cell Panel can contain a Room.
             if(positionIsValid(cellPanel)) {
-                cellPanel.addImage(ROOM_IMAGE);
+                cellPanel.addImage(images.getImagePath("Room"));
                 cellPanel.removeBorder();
             } else {
-                cellPanel.addImage(INVALID_ROOM_IMAGE);
+                cellPanel.addImage(images.getImagePath("InvalidRoom"));
                 cellPanel.removeBorder();
             }
         }
     }
-    
+
+    /**
+     * Removes the image and restores the default border to the Cell, if the
+     * Cell contains no Entity.
+     * @param cell
+     * @param event The event that originated the method call.
+     */
     @Override
     public void mouseExited(Cell cell, MouseEvent event) {
         CellPanel cellPanel = (CellPanel) event.getSource();
-        if (cell.getEntity().equals(Entity.NO_ENTITY)) {
+        if (cellPanel.getCell().getEntity() == null) {
             cellPanel.removeImage();
             cellPanel.restoreDefaultBorder();
         }
     }
 
+    /**
+     * Controls the behaviour of the tool when the user presses a mouse button.
+     * @param cell
+     * @param event The event that originated the method call.
+     */
     @Override
     public void mousePressed(Cell cell, MouseEvent event) {
-        doRoom(cell, event);
-    }
-
-    private void doRoom(Cell cell, MouseEvent event) {
         CellPanel cellPanel = (CellPanel) event.getSource();
+        // Delete the Room from the Cell upon right mouse click.
         if (SwingUtilities.isRightMouseButton(event)) {
-            deleteRoom(cell, cellPanel);
+            new DeleteEntity(cellPanel).execute();
+            // If there are no more rooms left, reset room placement restriction.
+            if(level.getRooms().size() == 0) {
+                firstRoomCell = null;
+            }
+        // Add a new Room to the Cell upon left mouse click.
         } else if (SwingUtilities.isLeftMouseButton(event) && positionIsValid(cellPanel)) {
-            cell.setRoom(new Room(cellPanel));
+            cell.setEntity(new Room(cellPanel));
+            // Check if a Room has already been created.
             if(firstRoomCell == null) {
+                // Sets the first room, governing where future rooms can be placed.
                 setFirstRoomCell(cell);
             }
         }
     }
 
-    public void deleteRoom(Cell cell, CellPanel cellPanel) {
-        cell.setEntity(Entity.NO_ENTITY);
-        removeDeadExits(cell);
-        // Visualise the delete immediately rather than waiting for mouseExited().
-        cellPanel.removeImage();
-        cellPanel.restoreDefaultBorder();
-        if(cellPanel.isSelected()) {
-            cellPanel.setDeselected();
-        }
-        if(level.getRooms() == null) {
-            firstRoomCell = null;
-        }
-    }
-
-    private void removeDeadExits(Cell cell) {
-        List<Room> destinations = new ArrayList<>();
-        for(Exit exit : cell.getRoom().getExits()) {
-            destinations.add(exit.getDestination());
-        }
-        Exit exitToRemove = null;
-        for(Room room : destinations) {
-            for(Exit exit : room.getExits()) {
-                if(exit.getDestination().equals(cell.getRoom())) {
-                    exitToRemove = exit;
-                }
-            }
-            room.removeExit(exitToRemove);
-        }
-        Map<String, Cell> adjacentCells = cell.getAdjacentCells();
-        for(Cell aCell : adjacentCells.values()) {
-            if(aCell.X != -1 && aCell.Y != -1) {
-                aCell.getCellPanel().removeImage();
-                aCell.getCellPanel().restoreDefaultBorder();
-                aCell.setEntity(Entity.NO_ENTITY);
-            }
-        }
-    }
-
-    private boolean positionIsValid(CellPanel cellPanel) {
-        if (firstRoomCell == null) {
-            return true;
-        }
-        return isEven(cellPanel.getX()) == xEven && isEven(cellPanel.getY()) == yEven;
-    }
-
+    /**
+     * Prevents rooms from being placed side by side.  Records the position of
+     * the first room placed within the level the parity of each of the x and
+     * y coordinates.
+     * @param cell The new Cell which contains the first Room in the Level.
+     */
     private void setFirstRoomCell(Cell cell) {
         this.firstRoomCell = cell;
         if (isEven(cell.X)) {
@@ -140,7 +129,27 @@ public class RoomTool implements MapTool {
         }
     }
 
+    /**
+     * Compares the parity of the coordinates of the CellPanel passed-in with
+     * the parity of the coordinates of the Cell which contains the first
+     * Room placed in the Level.
+     * @param cellPanel The CellPanel whose parity you want to test.
+     * @return True if the parity matches, otherwise false.
+     */
+    private boolean positionIsValid(CellPanel cellPanel) {
+        if (firstRoomCell == null) {
+            return true;
+        }
+        return isEven(cellPanel.getX()) == xEven && isEven(cellPanel.getY()) == yEven;
+    }
+
+    /**
+     * Determines the parity of the passed-in integer.
+     * @param coordinate The coordinate under test.
+     * @return True if parity is even, otherwise false.
+     */
     private boolean isEven(int coordinate) {
         return coordinate % 2 == 0;
     }
+
 }
