64eb90724f28d0a63f69013cbad72e5843608008
fado <pdonnelly19@qub.ac.uk>
2014-08-15 12:33:18 +0100

    Rejigged some of the tests to work with refactored code.

--- test/data/DirectionTest.java
+++ test/data/DirectionTest.java
@@ -1,7 +1,24 @@
 package data;
 
-import data.Direction;
-import data.Cell;
+/**
+ * JDig, a tool for the automatic generation of LPC class files for Epitaph
+ * developers.
+ * Copyright (C) 2014 Fado@Epitaph.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.awt.Point;
 import static org.junit.Assert.*;
 import org.junit.Before;
@@ -227,4 +244,5 @@ public class DirectionTest {
         int actual = Direction.SOUTHEAST.translateY(testCell);
         assertEquals(expected, actual);
     }
+
 }
