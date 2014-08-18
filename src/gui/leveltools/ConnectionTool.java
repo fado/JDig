dcb5c6ea99a043f5793873389aef3a8b20642ae5
fado <pdonnelly19@qub.ac.uk>
2014-08-18 12:54:09 +0100

    Added Javadoc.  Handed responsibility for Connection deletion over to the Deletion tool.

--- src/gui/leveltools/ConnectionTool.java
+++ src/gui/leveltools/ConnectionTool.java
@@ -22,53 +22,78 @@ package gui.leveltools;
 import data.Cell;
 import data.Connection;
 import data.ConnectionType;
-import data.ExitBuilder;
 import data.Entity;
 import gui.CellPanel;
-
 import java.awt.event.MouseEvent;
 import javax.swing.SwingUtilities;
 import javax.swing.border.Border;
 
-public class ExitTool extends LevelTool {
+/**
+ * This class controls the addition and removal of Connections from a Level.
+ */
+public class ConnectionTool implements LevelTool {
 
     ConnectionType connectionType;
-    
+    private CellTool cellTool = new CellTool();
+
+    /**
+     * Controls what image should be displayed when the user mouses over the Cell.
+     * @param cell
+     * @param event The event that originated the method call.
+     */
     @Override
     public void mouseEntered(Cell cell, MouseEvent event) {
         CellPanel cellPanel = (CellPanel)event.getSource();
         Entity entity = cell.getEntity();
         connectionType = cell.getPotentialConnectionType();
-        
+
+        // Check there isn't an Entity in the Cell already.
         if(entity == null) {
+            // Check the potential connection type isn't NONE.
             if(connectionType != ConnectionType.NONE) {
-                cellPanel.addImage(connectionType.getPath());
+                cellTool.addImage(cellPanel, connectionType.getPath());
                 Border border = new Connection(connectionType).getBorder();
                 cellPanel.setBorder(border);
             }
         }
     }
 
+    /**
+     * Removes the image and restores the default border to the Cell, if the Cell
+     * contains no Entity.
+     * @param cell
+     * @param event The event that originated the method call.
+     */
     @Override
     public void mouseExited(Cell cell, MouseEvent event) {
         CellPanel cellPanel = (CellPanel)event.getSource();
         Entity entity = cell.getEntity();
         if (entity == null) {
-            cellPanel.removeImage();
-            cellPanel.restoreDefaultBorder();
+            cellTool.removeImage(cellPanel);
+            cellTool.restoreDefaultBorder(cellPanel);
         }
     }
 
+    /**
+     * Controls the behaviour of the tool when the user presses a mouse button.
+     * @param cell
+     * @param event The event that originated the method call.
+     */
     @Override
     public void mousePressed(Cell cell, MouseEvent event) {
         doExit(cell, event);
     }
 
+    /**
+     * Creates a new Connection in the Cell and instructs the ExitBuilder to
+     * build appropriate Exits in the surrounding rooms.
+     * @param cell
+     * @param event The event that originated the method call.
+     */
     private void doExit(Cell cell, MouseEvent event) {
         if (SwingUtilities.isRightMouseButton(event)) {
-            if (cell.isExit()) {
-                cell.setEntity(null);
-            }
+            CellPanel cellPanel = (CellPanel)event.getSource();
+            new DeletionTool().deleteEntity(cellPanel);
         } else if (SwingUtilities.isLeftMouseButton(event)) {
             ConnectionType connectionType = cell.getPotentialConnectionType();
             if (connectionType != ConnectionType.NONE && !cell.isExit()) {
