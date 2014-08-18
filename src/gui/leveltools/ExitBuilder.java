b9175fc35d60eaac5acf0354adffb7770991a2fc
fado <pdonnelly19@qub.ac.uk>
2014-08-18 13:01:58 +0100

    Moved to a new directory.  Added Javadoc.

--- src/gui/leveltools/ExitBuilder.java
+++ src/gui/leveltools/ExitBuilder.java
@@ -1,4 +1,4 @@
-package data;
+package gui.leveltools;
 
 /**
  * JDig, a tool for the automatic generation of LPC class files for Epitaph 
@@ -19,17 +19,28 @@ package data;
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+import data.Cell;
+import data.ConnectionType;
+import data.Direction;
+import data.Exit;
+import data.ExitType;
+import data.Room;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Builds exits/adds exits to Rooms.
+ */
 public class ExitBuilder {
 
     private static Map<String, Cell> cells;
 
-    private ExitBuilder() {
-
-    }
-
+    /**
+     * Build exits for the passed-in cell.  Looks at the potential connection
+     * type of the passed-in cell then builds exits appropriately, depending
+     * on the direction.
+     * @param cell The cell around which exits should be built.
+     */
     public static void build(Cell cell) {
         ConnectionType connectionType = cell.getPotentialConnectionType();
         cells = cell.getAdjacentCells();
@@ -54,35 +65,61 @@ public class ExitBuilder {
         }
     }
 
+    /**
+     * Public method allowing the building of exits only along the forward diagonal
+     * axis (southwest/northeast).
+     * @param cell
+     */
     public static void buildOnlyForwardDiagonal(Cell cell) {
         cells = cell.getAdjacentCells();
         buildForwardDiagonal();
     }
 
+    /**
+     * Public method allowing the building of exits only along the backward diagonal
+     * axis (southeast/northwest).
+     * @param cell
+     */
     public static void buildOnlyBackwardDiagonal(Cell cell) {
         cells = cell.getAdjacentCells();
         buildBackwardDiagonal();
     }
 
+    /**
+     * Builds a forward diagonal exit (southwest/northeast).
+     */
     private static void buildForwardDiagonal() {
         buildExit("northeastCell", "southwestCell", Direction.SOUTHWEST);
         buildExit("southwestCell", "northeastCell", Direction.NORTHEAST);
     }
 
+    /**
+     * Builds a backward diagonal exist (southeast/northwest).
+     */
     private static void buildBackwardDiagonal() {
         buildExit("northwestCell", "southeastCell", Direction.SOUTHEAST);
         buildExit("southeastCell", "northwestCell", Direction.NORTHWEST);
     }
 
+    /**
+     * Builds an exit in the origin room.
+     * @param origin The name of the origin cell.
+     * @param destination The name of the destination cell.
+     * @param direction The direction in which the exit should travel.
+     */
     private static void buildExit(String origin, String destination, Direction direction) {
+        // Get the origin room from the list of adjacent cells.
         Room originRoom = (Room)cells.get(origin).getEntity();
         List<Exit> exits = originRoom.getExits();
+        // Check to see if an exit already exists going in the passed-in direction.
         for (Exit exit : exits) {
             if (exit.getDirection() == direction) {
                 return;
             }
         }
+        // Get the destination room.
         Room destinationRoom = (Room)cells.get(destination).getEntity();
+        // Add the exit to the origin room.
         originRoom.addExit(new Exit(direction, destinationRoom, ExitType.PATH));
     }
 
