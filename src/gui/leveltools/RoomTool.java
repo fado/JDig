556004737384eb8aea66b54d9437149e9c578e42
fado <pdonnelly19@qub.ac.uk>
2014-08-18 13:02:48 +0100

    Added Javadoc.  Renamed some methods.

--- src/gui/leveltools/RoomTool.java
+++ src/gui/leveltools/RoomTool.java
@@ -24,20 +24,20 @@ import data.Level;
 import data.Room;
 import gui.CellPanel;
 import properties.Images;
-
 import java.awt.event.MouseEvent;
 import javax.swing.SwingUtilities;
 
 /**
  * This class controls the addition and removal of Rooms from a Level.
  */
-public class RoomTool extends LevelTool {
+public class RoomTool implements LevelTool {
 
     private Cell firstRoomCell;
     private boolean xEven = false;
     private boolean yEven = false;
     private Level level;
     private Images images = new Images();
+    private CellTool levelTool = new CellTool();
 
     /**
      * Default constructor.
@@ -63,11 +63,11 @@ public class RoomTool extends LevelTool {
         if (cellPanel.getCell().getEntity() == null) {
             // Check whether or not the Cell Panel can contain a Room.
             if(positionIsValid(cellPanel)) {
-                cellPanel.addImage(images.getImagePath("Room"));
-                cellPanel.removeBorder();
+                levelTool.addImage(cellPanel, images.getImagePath("Room"));
+                levelTool.removeBorder(cellPanel);
             } else {
-                cellPanel.addImage(images.getImagePath("InvalidRoom"));
-                cellPanel.removeBorder();
+                levelTool.addImage(cellPanel, images.getImagePath("InvalidRoom"));
+                levelTool.removeBorder(cellPanel);
             }
         }
     }
@@ -82,8 +82,8 @@ public class RoomTool extends LevelTool {
     public void mouseExited(Cell cell, MouseEvent event) {
         CellPanel cellPanel = (CellPanel) event.getSource();
         if (cellPanel.getCell().getEntity() == null) {
-            cellPanel.removeImage();
-            cellPanel.restoreDefaultBorder();
+            levelTool.removeImage(cellPanel);
+            levelTool.restoreDefaultBorder(cellPanel);
         }
     }
 
@@ -97,20 +97,31 @@ public class RoomTool extends LevelTool {
         CellPanel cellPanel = (CellPanel) event.getSource();
         // Delete the Room from the Cell upon right mouse click.
         if (SwingUtilities.isRightMouseButton(event)) {
-            new DeleteEntity(cellPanel).execute();
+            new DeletionTool().deleteEntity(cellPanel);
             // If there are no more rooms left, reset room placement restriction.
-            if(level.getRooms().size() == 0) {
-                firstRoomCell = null;
+            if(level.getRooms().isEmpty()) {
+                resetPlacementRestriction();
             }
         // Add a new Room to the Cell upon left mouse click.
         } else if (SwingUtilities.isLeftMouseButton(event) && positionIsValid(cellPanel)) {
-            cell.setEntity(new Room(cellPanel));
+            Room room = new Room(cellPanel);
+            cell.setEntity(room);
+            level.registerRoom(room);
             // Check if a Room has already been created.
             if(firstRoomCell == null) {
                 // Sets the first room, governing where future rooms can be placed.
-                setFirstRoomCell(cell);
+                setPlacementRestriction(cell);
             }
         }
+    }
+
+    /**
+     * Resets the Cell containing the first room and the xEven and yEven booleans.
+     */
+    private void resetPlacementRestriction() {
+        firstRoomCell = null;
+        xEven = false;
+        yEven = false;
     }
 
     /**
@@ -119,7 +130,7 @@ public class RoomTool extends LevelTool {
      * y coordinates.
      * @param cell The new Cell which contains the first Room in the Level.
      */
-    private void setFirstRoomCell(Cell cell) {
+    private void setPlacementRestriction(Cell cell) {
         this.firstRoomCell = cell;
         if (isEven(cell.X)) {
             xEven = true;
