ab6df08bdb14b2f2e15d8081b567d28ea466a023
fado <pdonnelly19@qub.ac.uk>
2014-08-19 11:29:47 +0100

    Decomposed the main method into a lot of smaller methods to make it easier to test.  Also added JavaDoc.

--- src/gen/LpcWriter.java
+++ src/gen/LpcWriter.java
@@ -21,7 +21,6 @@ package gen;
 
 import data.Exit;
 import data.Room;
-
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -30,32 +29,79 @@ import java.io.Writer;
 import java.nio.charset.Charset;
 import java.text.MessageFormat;
 import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import properties.JdigProperties;
 
+/**
+ * Takes a Room object and produces an LPC file.
+ */
 public class LpcWriter {
 
     static final Logger logger = LoggerFactory.getLogger(LpcWriter.class);
-    private final String TEMPLATE_PATH = "./resources/templates/room.template";
-    private Writer writer;
-    
-    public void write(Room room) throws IOException {
-        Template template = new Template();
-        template.load(TEMPLATE_PATH);
-        List<String> lines = template.getLines();
-        logger.info("Getting lines...");
 
-        String output = "";
+    /**
+     * Generates an LPC file for the passed-in Room.
+     * @param room The Room for which you want to generate an LPC file.
+     * @throws IOException
+     */
+    public void generate(Room room) throws IOException {
+        String output = getTemplateString();
+        output = replaceRoomStrings(output, room);
+        output = replaceExitStrings(output, getExitString(room));
+        write(output, room);
+    }
 
-        for(String string : lines) {
-            output += string + "\n";
-        }
+    /**
+     * Replaces the exit marker in the room template with the exit String passed-in.
+     * @param output The output string you want to add the exits too.
+     * @param exitString The exit string you want to add.
+     * @return The new output string with the exits added.
+     */
+    private String replaceExitStrings(String output, String exitString) {
+        output = output.replace("%exit%", exitString);
+        return output;
+    }
+
+    /**
+     * Replaces all the property markers in the output string with properties taken
+     * from the Room file.
+     * @param output The output string you want to add the properties too.
+     * @param room The Room from which you want to take the properties.
+     * @return The new output string with the properties added.
+     */
+    private String replaceRoomStrings(String output, Room room) {
         output = output.replace("%short%", room.getShort());
         output = output.replace("%determinate%", room.getDeterminate());
         output = output.replace("%long%", room.getLong());
         output = output.replace("%light%", ""+room.getLight());
+        return output;
+    }
 
+    /**
+     * Writes an LPC file.
+     * @param output The output String containing the contents of the LPC file.
+     * @param room The Room for which you are writing the LPC file.
+     */
+    private void write(String output, Room room) {
+        try {
+            Writer writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream("./lpc/" + room.getName() + ".c"),
+                    Charset.defaultCharset()));
+            writer.write(output);
+            writer.flush();
+            logger.info("Writing LPC file for {0}", room.getName());
+        } catch (IOException ex) {
+            logger.error(ex.toString());
+        }
+    }
+
+    /**
+     * Generates a String based on the Exits contained in the Room.
+     * @param room The Room for which you are generating Exits.
+     * @return a String based on the Exits contained in the Room.
+     */
+    private String getExitString(Room room) {
         String exits = "";
         for (Exit exit : room.getExits()) {
             exits += MessageFormat.format(
@@ -65,19 +111,28 @@ public class LpcWriter {
                     exit.getExitType().toString().toLowerCase()
             );
         }
+        return exits;
+    }
 
-        output = output.replace("%exit%", exits);
+    /**
+     * Generates a starting output String based on the template file
+     * defined in config.properties.
+     * @return a starting output String based on the template file.
+     * @throws IOException
+     */
+    private String getTemplateString() throws IOException {
+        Template template = new Template();
+        JdigProperties properties = new JdigProperties();
+        template.load(properties.get("Template"));
 
-        try {
-            writer = new BufferedWriter(new OutputStreamWriter(
-                    new FileOutputStream("./lpc/"+ room.getName() +".c"),
-                    Charset.defaultCharset()));
-            writer.write(output);
-            writer.flush();
-            logger.info("Writing LPC file for {0}", room.getName());
-        } catch (IOException ex) {
-            logger.error(ex.toString());
+        logger.info("Getting lines...");
+
+        List<String> lines = template.getLines();
+        String output = "";
+        for(String string : lines) {
+            output += string + "\n";
         }
+        return output;
     }
     
 }
