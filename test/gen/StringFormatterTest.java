b8c3d5f9ce0249e5d82483e9185b7a3f464a28f5
fado <pdonnelly19@qub.ac.uk>
2014-08-29 13:23:29 +0100

    Added GPL.

--- test/gen/StringFormatterTest.java
+++ test/gen/StringFormatterTest.java
@@ -1,5 +1,24 @@
 package gen;
 
+/**
+ * JDig, a tool for the automatic generation of LPC class files for Epitaph
+ * developers.
+ * Copyright (C) 2014 Fado@Epitaph.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import data.Direction;
 import data.Exit;
 import data.ExitType;
@@ -8,26 +27,18 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
-
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.charset.Charset;
-import java.util.Properties;
-
 import static junit.framework.TestCase.assertEquals;
 
-/**
- * Created by Administrator on 27/08/2014.
- */
 public class StringFormatterTest {
 
     Room room, room2;
     StringFormatter stringFormatter;
-    Properties testProperties;
     String path;
 
     @Rule
