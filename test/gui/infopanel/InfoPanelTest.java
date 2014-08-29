b4425081c727fa89c605d408412d17e1bda5fa24
fado <pdonnelly19@qub.ac.uk>
2014-08-29 11:59:19 +0100

    Added GPL.

--- test/gui/infopanel/InfoPanelTest.java
+++ test/gui/infopanel/InfoPanelTest.java
@@ -1,5 +1,23 @@
 package gui.infopanel;
 
+/**
+ * JDig, a tool for the automatic generation of LPC class files for Epitaph
+ * developers. Copyright (C) 2014 Fado@Epitaph.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import data.Cell;
 import data.Level;
 import data.Room;
@@ -7,30 +25,21 @@ import data.Street;
 import gui.levelpanel.CellPanel;
 import org.junit.Before;
 import org.junit.Test;
-
+import utils.TestingUtils;
 import javax.swing.JComboBox;
-import javax.swing.JPanel;
-import java.awt.Component;
 import java.awt.Point;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import static junit.framework.Assert.assertNotNull;
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-/**
- * Created by Administrator on 29/08/2014.
- */
 public class InfoPanelTest {
 
     Level level;
     InfoPanel infoPanel;
     CellPanel cellPanel;
     Room room;
+    TestingUtils testingUtils = new TestingUtils();
 
     @Before
     public void setUp() {
@@ -60,37 +69,11 @@ public class InfoPanelTest {
         level.addStreet(new Street("foo"));
         infoPanel.populateStreetNames();
 
-        JComboBox streetNameField = getStreetNameField();
+        JComboBox streetNameField = testingUtils.getStreetNameField(infoPanel);
         if (streetNameField != null) {
             assertTrue(streetNameField.getItemCount() > 0);
         } else {
             fail();
         }
-    }
-
-    private JComboBox getStreetNameField() {
-        // And so we embark on a magical journey.
-        // Get the content panel.
-        Component[] components = infoPanel.getComponents();
-        JPanel contentPanel = null;
-        for(Component component : components) {
-            if(component.getName() != null
-                    && component.getName().equals("contentPanel")) {
-                contentPanel = (JPanel)component;
-            }
-        }
-        // Get the streetNameField.
-        Component[] internalComponents;
-        JComboBox streetNameField = null;
-        if (contentPanel != null) {
-            internalComponents = contentPanel.getComponents();
-            for(Component component : internalComponents) {
-                if(component.getName() != null
-                        && component.getName().equals("streetNameField")) {
-                    streetNameField = (JComboBox)component;
-                }
-            }
-        }
-        return streetNameField;
     }
 }
