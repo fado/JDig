ed0f5e92fccc93baeee7e2c84639a52b7a3e20f5
fado <pdonnelly19@qub.ac.uk>
2014-08-29 17:00:48 +0100

    Some method signatures changed.

--- test/gui/levelpanel/CellPanelTest.java
+++ test/gui/levelpanel/CellPanelTest.java
@@ -1,5 +1,24 @@
 package gui.levelpanel;
 
+/**
+ * JDig, a tool for the automatic generation of LPC class files for Epitaph
+ * developers.
+ * Copyright (C) 2014 Fado@Epitaph.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import data.Cell;
 import data.Connection;
 import data.ConnectionType;
@@ -8,9 +27,7 @@ import data.Room;
 import org.junit.Before;
 import org.junit.Test;
 import properties.Images;
-
 import java.awt.Point;
-
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
