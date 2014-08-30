2f618b7cd91b75a2268a1f26a552a4dacb018e04
fado <pdonnelly19@qub.ac.uk>
2014-08-30 17:06:22 +0100

    Factored the ActionListener for adding a bug report out into its own class.

---
+++ src/gui/menubar/BugReportActionListener.java
@@ -0,0 +1,36 @@
+package gui.menubar;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import properties.JdigProperties;
+
+import java.awt.Desktop;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class BugReportActionListener implements ActionListener {
+
+    static final Logger logger = LoggerFactory.getLogger(BugReportActionListener.class);
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        Desktop desktop = null;
+        if (Desktop.isDesktopSupported()) {
+            desktop = Desktop.getDesktop();
+        }
+        if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+            JdigProperties jdigProperties = new JdigProperties();
+            URL url;
+            try {
+                url = new URL(jdigProperties.getProperty("BugReportURL"));
+                desktop.browse(url.toURI());
+            } catch (URISyntaxException | IOException ex) {
+                logger.error(ex.toString());
+            }
+
+        }
+    }
+}
