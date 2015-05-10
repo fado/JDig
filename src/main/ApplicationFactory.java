462fc34c781c6413c5feabfce9391c9362c3a429
Fado <pdonnelly19@qub.ac.uk>
2015-05-10 14:50:23 +0100

    Initialising a brand new ApplicationFactory.

---
+++ src/main/ApplicationFactory.java
@@ -0,0 +1,19 @@
+package main;
+
+import data.Level;
+
+/**
+ * Created by Fado on 10/05/2015 for the Epitaph MUD.
+ * Copyright (C) 2015 Fado@Epitaph.
+ * Distributed under the GPL3 license.
+ */
+public enum ApplicationFactory {
+    INSTANCE;
+
+    private Level level;
+
+    public void newLevel() { this.level = new Level(); }
+
+    public Level getLevel() { return this.level; }
+
+}
