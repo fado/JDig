ae152729a76f8cbc47f6f802b38666d60c069be2
Fado <pdonnelly19@qub.ac.uk>
2015-05-16 10:21:42 +0100

    Added the ExitType enum here, rather than in an external class.  Also, removed the external class.

--- src/data/Exit.java
+++ src/data/Exit.java
@@ -1,27 +1,5 @@
 package data;
 
-/**
- * Jdig, a tool for the automatic generation of LPC class files for Epitaph
- * developers.
- * Copyright (C) 2014 Fado@Epitaph.
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
-/**
- * An Exit, linking one Room to another Room.
- */
 public class Exit {
 
     public enum ExitType {
@@ -37,38 +15,20 @@ public class Exit {
     private final Room destination;
     private final ExitType exitType;
 
-    /**
-     * Constructor.
-     * @param direction The Direction the Exit is pointing.
-     * @param destination The Room to which the Exit is pointing.
-     * @param exitType The type of Exit, in MUD terms.
-     */
     public Exit(Direction direction, Room destination, ExitType exitType) {
         this.direction = direction;
         this.destination = destination;
         this.exitType = exitType;
     }
 
-    /**
-     * Returns the Direction of the Exit.
-     * @return the Direction of the Exit.
-     */
     public Direction getDirection() {
         return this.direction;
     }
 
-    /**
-     * Returns the Room that is the Destination of the Exit.
-     * @return the Room that is the Destination of the Exit.
-     */
     public Room getDestination() {
         return this.destination;
     }
 
-    /**
-     * Returns the ExitType.
-     * @return the ExitType
-     */
     public ExitType getExitType() {
         return this.exitType;
     }
