3e35b4d5cd7eda563eab6d1197aad0b1ee6cf901
Fado <pdonnelly19@qub.ac.uk>
2015-05-16 10:21:12 +0100

    Removed all the annoying Javadoc.  Added the new GPL notice.

--- src/data/ConnectionType.java
+++ src/data/ConnectionType.java
@@ -1,28 +1,11 @@
 package data;
 
-/**
- * Jdig, a tool for the automatic generation of LPC class files for Epitaph
- * developers.
- * Copyright (C) 2014 Fado@Epitaph.
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
 import properties.ImageProperties;
 
 /**
- * Represents the types of Connection that can exist between rooms.
+ * Created by Fado on 12/05/2015 for the Epitaph MUD.
+ * Copyright (C) 2015 Fado@Epitaph.
+ * Distributed under the GPL3 license.
  */
 public enum ConnectionType {
     NONE(null),
@@ -34,18 +17,10 @@ public enum ConnectionType {
 
     private String exitImagePath;
 
-    /**
-     * Constructor.
-     * @param exitImagePath The path where the associated image lies.
-     */
     ConnectionType(String exitImagePath) {
          this.exitImagePath = exitImagePath;
     }
 
-    /**
-     * Returns the path of the image associated with the ConnectionType.
-     * @return the path of the image associated with the ConnectionType.
-     */
     public String getPath() {
         return this.exitImagePath;
     }
